d6a6391702c233e1494f76a2ad561933c3023a81
rachel-joy <racheljvoth@gmail.com>
2018-03-21 21:33:50 -0700

    Created blog index page for /blog path
    This is eventually where most recent blog posts will be displayed in a
    list with links to individual posts. For now, I'm setting up basic input
    boxes for a new post. I want to try and get postgraphile working.

---
+++ pages/blog/index.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div>
+    <h3>Create a New Post</h3>
+    <br>
+    <input v-model="title" type="text" name="title" value="" placeholder="title">
+    <br>
+    <input v-model="description" type="text" name="description" value="" placeholder="description">
+    <br>
+    <input v-model="content" type="text" name="content" value="" placeholder="content">
+    <br>
+    <br>
+    <nuxt-link :to="'/'">Home</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      description: '',
+      content: '',
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
