41520a1667fc022d8af280dfb3866a12734fe47d
rachel-joy <racheljvoth@gmail.com>
2018-04-03 17:29:29 -0700

    Include CORS in postgraphile bash script

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo postgraphile -c postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB --schema $SCHEMA --default-role $DEFAULT_ROLE --port $PORT --host $HOST --watch
-postgraphile -c postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB --schema $SCHEMA --default-role $DEFAULT_ROLE --port $PORT --host $HOST --watch 
+echo postgraphile -c postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB --schema $SCHEMA --default-role $DEFAULT_ROLE --port $PORT --host $HOST --cors --watch
+postgraphile -c postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB --schema $SCHEMA --default-role $DEFAULT_ROLE --port $PORT --host $HOST --cors --watch
